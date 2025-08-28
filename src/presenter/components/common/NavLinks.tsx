2081bdf12c370bbd516b3cf4b0dd2bc2bc604b3d
lorainep <lou.hoffmann@gmail.com>
2025-08-28 23:29:29 +0200

    test height mobile

--- src/presenter/components/common/NavLinks.tsx
+++ src/presenter/components/common/NavLinks.tsx
@@ -97,7 +97,7 @@ export const NavBarSection: React.FC<NavBarProps> = ({ addBtn }) => {
             {/* CONTAINER */}
             <div className={
                 (navBottom ?
-                    `items-center opacity-100 anim ${color}BG backdropBlur wRespXL justify-center relative bottom-0 gap-6` :
+                    `items-center opacity-100 anim ${color}BG backdropBlur wRespXL justify-center  bottom-0 gap-6 sticky` :
                     'z-0 md:gap-6 gap-2') +
                 ` flex z-30 px-4`
             }>
