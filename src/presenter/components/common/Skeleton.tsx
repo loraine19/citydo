3c35f4bd5846f790efb2db7535a75db78dfa2023
lorainep <lou.hoffmann@gmail.com>
2025-09-02 21:48:57 +0200

    resize

--- src/presenter/components/common/Skeleton.tsx
+++ src/presenter/components/common/Skeleton.tsx
@@ -9,14 +9,14 @@ export function Skeleton(props: { className?: string }) {
     return (
         <div className={`h-full  pb-1.5 w-full flex items-center justify-center ${className ?? ''}`}>
             <Card className={style + 'fixCard w-full h-full gap-2 !flex flex-col'}>
-                <CardHeader className={`${color}BG bg-slate-300 mx-4 FixCardHeader animate-pulse !flex h-full flex-1 w-[calc(100%-1rem)] border-slate-300 lg:min-h-[220px] md:min-h-[150px] min-h-[200px]`}>
+                <CardHeader className={`${color}BG bg-slate-300/70 mx-4 FixCardHeader animate-pulse !flex h-full flex-1 w-[calc(100%-1rem)] border-slate-300 lg:min-h-[220px] md:min-h-[150px] min-h-[200px]`}>
                 </CardHeader>
                 <CardBody className="FixCardBody flex-1 !py-4 !flex gap-2">
                     <div className="h-6 bg-slate-300 rounded-2xl  animate-pulse"></div>
                     <div className="h-4 bg-slate-300 rounded-2xl w-3/4 animate-pulse"></div>
 
                 </CardBody>
-                <CardFooter className="FixCardFooter !flex gap-2 mt-2 px-6 justify-between items-center">
+                <CardFooter className="FixCardFooter !flex gap-2 !my-2 px-6 justify-between items-center">
                     <div className="h-8 bg-slate-300 rounded-2xl w-1/2 animate-pulse"></div>
                     <div className="h-8 bg-slate-300 rounded-2xl w-8 animate-pulse"></div>
                 </CardFooter>
