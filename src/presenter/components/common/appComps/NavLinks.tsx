66b39355d7c5a645b3df920e33ab47829c14cf70
Loraine P <lou.hoffmann@gmail.com>
2025-09-23 16:16:23 +0200

    Refactor NavLinks component props handling

--- src/presenter/components/common/appComps/NavLinks.tsx
+++ src/presenter/components/common/appComps/NavLinks.tsx
@@ -14,7 +14,7 @@ interface NavBarProps {
     color?: string;
 }
 
-export const NavLinks: React.FC<NavBarProps> = ({ listPage=true, placement, mainPage = true}) => 
+export const NavLinks: React.FC<NavBarProps> = ({ listPage, placement, mainPage }) => 
     const location = useLocation()
     const type = new URLSearchParams(location.pathname.split("/")[1]).toString().replace("=", '');
     const { setColor, color, hideNavBottom } = useUxStore((state) => state);
@@ -145,7 +145,7 @@ export const NavLinks: React.FC<NavBarProps> = ({ listPage=true, placement, main
                         )
                     })}
                 </NavigationBar>
-                {(listPage || mainPage) &&
+                {(listPage || mainPage || type ) &&
                     <FabMenu
                         backdropBlur={true}
                         open={openFab}
