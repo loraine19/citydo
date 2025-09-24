041115341a91adf74ea8a3271782cac34e62c1ff
lorainep <lou.hoffmann@gmail.com>
2025-09-24 19:23:45 +0200

    up

--- src/presenter/components/shared/base/baseComps/DialogImage.tsx
+++ src/presenter/components/shared/base/baseComps/DialogImage.tsx
@@ -14,7 +14,7 @@ const DialogImage: React.FC<DialogImageProps> = ({ image, alt = '', onClose }) =
         open={!!image} className="p-4">
         <CardMD className="!max-w-[80vw] !max-h-[70dvh] h-full w-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] overflow-hidden p-2  bg-clip">
             <img
-                src={image} alt={alt} className="w-auto h-full rounded-2xl max-h-[70dvh] object-fill" />
+                src={image} alt={alt} className="w-auto h-full rounded-2xl max-h-[70dvh] object-contain" />
         </CardMD>
     </BackDropBlur>
 );
