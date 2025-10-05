7f6b0fec7a3bef3cdc05aac35c1f349147b7e7bc
lorainep <lou.hoffmann@gmail.com>
2025-10-05 19:02:54 +0200

    fix: adjust icon scaling in MoreButton and update opacity in AvatarStack Chip component

--- src/presenter/components/common/moreBtn.tsx
+++ src/presenter/components/common/moreBtn.tsx
@@ -59,7 +59,7 @@ export const MoreButton = ({ id, type, flagged, title, className }: moreButtonPr
                 trigger={
                     <Icon
                         fill
-                        style='!font-extrabold !scale-x-[1.2] !p-1 z-[4] h-[1.5rem] md3-stroke max-h-max'
+                        style='!font-extrabold !scale-x-[1.1] !p-1 z-[4] h-[1.5rem] md3-stroke max-h-max'
                         color={''}
                         textIcon="⋮"
                         size={'3xl'}
