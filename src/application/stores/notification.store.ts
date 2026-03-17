ba00f1cb66ea6869311aa41e7083d2765c496551
lorainep <lou.hoffmann@gmail.com>
2026-03-17 20:47:28 +0100

    fix: reorder import statements for consistency in notification store

--- src/application/stores/notification.store.ts
+++ src/application/stores/notification.store.ts
@@ -1,10 +1,10 @@
 // src/application/stores/notificationStore.ts
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
-import { cryptedStorage } from '../../infrastructure/services/storageService';
 import DI from '../../di/ioc';
 import { ElementNotif, Notif } from '../../domain/entities/Notif';
 import { NotifView } from '../../presenter/views/viewsEntities/notifViewEntity';
+import { cryptedStorage } from '../../infrastructure/services/storageService';
 
 interface NotificationStore {
   notifList: NotifView[];
